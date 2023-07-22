e8d9fbd49f415e92ebf0f1ea66ce9b6fe649ce49
hurgyunam <heokyunam@naver.com>
2023-07-22 12:26:43 +0900

    프로젝트 생성

---
+++ components/stage/Card.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card">
+        Card
+    </div>
+</template>
+<script lang='ts' setup>
+    
+</script>
+<style lang='scss'>
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <StageContainerApplicants />
+</template>
+<script lang="ts" setup>
+
+</script>
