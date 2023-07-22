97b11facd886b8ae06d58daa3aa4a04ae4a4271f
hurgyunam <heokyunam@naver.com>
2023-07-22 12:32:28 +0900

    더미 추가. 인물 6명

---
+++ composables/dummy.ts
@@ -0,0 +1,46 @@
+
+export interface EmployeeStats {
+    name: string;
+    image_url: string;
+    social_enery: number;
+    develop_energy: number;
+}
+
+export const dummyEmployeeStats: EmployeeStats[] = [
+    {
+        name: "James",
+        image_url: "",
+        social_enery: 3,
+        develop_energy: 0,
+    },
+    {
+        name: "Liam",
+        image_url: "",
+        social_enery: 0,
+        develop_energy: 3,
+    },
+    {
+        name: "Lucas",
+        image_url: "",
+        social_enery: 1,
+        develop_energy: 2,
+    },
+    {
+        name: "Emma",
+        image_url: "",
+        social_enery: 2,
+        develop_energy: 1,
+    },
+    {
+        name: "Ava",
+        image_url: "",
+        social_enery: 3,
+        develop_energy: 0,
+    },
+    {
+        name: "Sophia",
+        image_url: "",
+        social_enery: 0,
+        develop_energy: 3,
+    },
+]
